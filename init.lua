4a255871b2b4e4b5e99ca0a5276908b0c11390cd
lvdongxu <lvdongxu@sjtu.edu.cn>
2023-06-05 21:00:46 +0800

    add gitsigns

--- init.lua
+++ init.lua
@@ -52,7 +52,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<C-i>', vim.lsp.buf.signature_help, opts)
 		vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
 		vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
 		vim.keymap.set('n', '<Leader>wl', function()
@@ -140,7 +140,12 @@ require("lazy").setup({
 		"tpope/vim-fugitive",
 		event = "VeryLazy",
 		cmd = "Git",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
 	}
+
 
 })
 -- colortheme
@@ -283,3 +288,47 @@ null_ls.setup({
 		end
 	end,
 })
+
+-- git signs
+require('gitsigns').setup {
+	signs                        = {
+		add          = { text = '│' },
+		change       = { text = '│' },
+		delete       = { text = '_' },
+		topdelete    = { text = '‾' },
+		changedelete = { text = '~' },
+		untracked    = { text = '┆' },
+	},
+	signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+	watch_gitdir                 = {
+		interval = 1000,
+		follow_files = true
+	},
+	attach_to_untracked          = true,
+	current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts      = {
+		virt_text = true,
+		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+	},
+	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+	sign_priority                = 6,
+	update_debounce              = 100,
+	status_formatter             = nil, -- Use default
+	max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+	preview_config               = {
+		-- Options passed to nvim_open_win
+		border = 'single',
+		style = 'minimal',
+		relative = 'cursor',
+		row = 0,
+		col = 1
+	},
+	yadm                         = {
+		enable = false
+	},
+}
