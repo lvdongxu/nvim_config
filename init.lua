6f6c0075ab983cab4e361121dd4abe23e0de2454
lvdongxu <lvdongxu@sjtu.edu.cn>
2023-06-05 22:16:45 +0800

    basic configuration done

--- init.lua
+++ init.lua
@@ -144,6 +144,16 @@ require("lazy").setup({
 	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {}
+		end,
 	}
 
 
@@ -332,3 +342,23 @@ require('gitsigns').setup {
 		enable = false
 	},
 }
+
+-- nvimtree
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
