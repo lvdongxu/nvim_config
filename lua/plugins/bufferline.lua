73be1fe019dee093115cc4e0906b5cabb8db055b
lvdongxu <lvdongxu@sjtu.edu.cn>
2023-06-11 22:24:56 +0800

    Add comment.nvim and update the bufferline signals

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -26,14 +26,14 @@ local M = {
                     diagnostics = "nvim_lsp",
                     -- 可选，显示 LSP 报错图标
                     ---@diagnostic disable-next-line: unused-local
-                    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                        local s = " "
-                        for e, n in pairs(diagnostics_dict) do
-                            local sym = e == "error" and " " or (e == "warning" and " " or "")
-                            s = s .. n .. sym
-                        end
-                        return s
-                    end,
+                    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                    --     local s = " "
+                    --     for e, n in pairs(diagnostics_dict) do
+                    --         local sym = e == "error" and " " or (e == "warning" and " " or "")
+                    --         s = s .. n .. sym
+                    --     end
+                    --     return s
+                    -- end,
                 },
             })
         end
